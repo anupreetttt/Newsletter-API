00aef3b405a74d12d57ee01ee4bd8fb55dbafe1b
Anupreet Paulkar <apaulk2@uic.edu>
2023-06-17 20:05:44 -0500

    Logging data

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const bodyParser = require('body-parser');
 const request = require('request')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile('/signup.html', {root: __dirname})
 });
+
+app.post('/', (req, res) => {
+
+    let firstName = (req.body.inputFirst);
+    let lastName = req.body.inputLast
+    let email = req.body.email
+
+    console.log(firstName, lastName, email);
+})
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
